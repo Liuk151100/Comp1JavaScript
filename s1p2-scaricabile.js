31b71f69f9340e414217d992307ec3166490feff
Luca Faini <lucafaini20@gmail.com>
2025-03-30 20:15:35 +0200

    Modificata la risposta dell'esercizio 2

--- s1p2-scaricabile.js
+++ s1p2-scaricabile.js
@@ -9,7 +9,7 @@ REGOLE
 
 /* ESERCIZIO 1
  Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
-
+*/
  let datatypeBoolean = true; //Questo datatype è di tipo Boolean e può avere solo due valori (true - false)
  let datatypeNumber = 12; //Questo datatype è di tipo Int e può contenere valori con e senza decimali 
  let datatypeBigInt = 1000000000000000; //Questo datatype è di tipo BigInt e serve a contenere valori interi molto grandi
@@ -17,14 +17,16 @@ REGOLE
 
 /* ESERCIZIO 2
  Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/
 
-
-let oggetto = { proprieta1: "proprieta1", proprieta2: "proprieta2" } //Gli oggetti in Javascript sono dei "contenitori" con al loro interno una serie di proprietà
+let oggetto = { proprieta1: "proprieta1", proprieta2: "proprieta2" } //Gli oggetti in Javascript sono dei "contenitori" con al 
+// loro interno una serie di proprietà (chiave-valore). Gli oggetti a loro volta possono essere inseriti all'interno di un array
+// e sono delimitati da delle parentesi graffe.
 
 
 /* ESERCIZIO 3
  Scriti il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
-
+*/
 
 let numero1 = 12
 let numero2 = 20
@@ -34,20 +36,20 @@ console.log(somma)
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
-
+*/
 
 let x = 12
 
 
 /* ESERCIZIO 5
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
-
+*/
 
 let name = "Luca"
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
-
+*/
 
 let risultatoDifferenza = x - 4
 console.log(risultatoDifferenza)
